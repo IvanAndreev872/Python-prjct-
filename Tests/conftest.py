4119c9d51fb116df62b5e924179ac1bee2df094a
andrey <rudakov.andrej1@yandex.ru>
2024-12-08 01:40:10 +0300

    Начал писать тесты для регистрации и мои записи

---
+++ Tests/conftest.py
@@ -0,0 +1,69 @@
+import pytest
+
+from aiogram import Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from sqlalchemy import create_engine
+from database import models
+from Tests.mocked_aiogram import MockedBot, MockedSession
+from app.middlewares.session_control import SessionControlMiddleware
+from sqlalchemy.orm import sessionmaker
+
+from app.handlers.welcome_handler import router as router_welcome
+from app.handlers.registration import router as router_registration
+from app.handlers.my_appointments import router as router_my_appointments
+
+all_routers = [router_welcome, router_registration, router_my_appointments]
+
+@pytest.fixture(scope='session')
+def engine():
+    engine = create_engine("sqlite+pysqlite:///:memory:", echo=False)
+    yield engine
+    engine.dispose()
+
+@pytest.fixture(scope='session')
+def tables(engine):
+    models.Base.metadata.create_all(engine)
+    yield
+    models.Base.metadata.drop_all(engine)
+
+@pytest.fixture(scope='function')
+def session(engine, tables):
+    connection = engine.connect()
+    transaction = connection.begin()
+    Session_mk = sessionmaker(bind=engine)
+    session = Session_mk()
+
+    yield session
+
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+attached = False
+
+@pytest.fixture(scope="function")
+def dp(session) -> Dispatcher:
+    dispatcher = Dispatcher(storage=MemoryStorage())
+    dispatcher.update.middleware(SessionControlMiddleware(session=session))
+    global attached
+    if not attached:
+        dispatcher.include_routers(*all_routers)
+        attached = True
+    yield dispatcher
+    dispatcher.storage.close()
+    dispatcher.shutdown()
+    dispatcher = None
+
+@pytest.fixture(scope="session")
+def bot() -> MockedBot:
+    #Несуществующий токен. Имеет такой вид, чтобы проходил валидацию
+    bot = MockedBot(token='7513777902:ABH7xoLvJtXg2-J9uC1XA3t2YT5zHlN5Jjk')
+    bot.session = MockedSession()
+    return bot
+
+@pytest.fixture(scope='function', autouse=True)
+def cleanup_database(session, dp, bot):
+    yield
+    for table in reversed(models.Base.metadata.sorted_tables):
+        session.execute(table.delete())
+    session.commit()
---
+++ app/handlers/my_appointments.py
@@ -0,0 +1,70 @@
+from aiogram import  F, Router
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import CallbackQuery
+from aiogram import types
+import database.db_utils as db_utils
+import app.keyboards.my_appointments_kb as my_appointments_kb
+
+router = Router()
+
+class AppointmentsStates(StatesGroup):
+    choosing_appointment = State()
+    pressing_button = State()
+
+
+@router.message(F.text.lower().strip() == 'мои записи')
+async def appointments_handler(message: types.Message, state: FSMContext):
+    user = db_utils.get_user_by_telegram_id(message.from_user.id)
+    appointments = db_utils.get_appointments_by_user(user)
+    kb = my_appointments_kb.get_appointments_kb(appointments)
+    await message.answer(text='Мои записи:', reply_markup=kb)
+    await state.set_state(AppointmentsStates.choosing_appointment)
+
+@router.callback_query(AppointmentsStates.choosing_appointment, F.data != 'main_menu')
+async def choosing_appointment_handler(callback: CallbackQuery, state: FSMContext):
+    appointment = db_utils.get_appointment_by_id(int(callback.data))
+    kb = my_appointments_kb.get_conf_cancel_kb(appointment)
+    service_name = db_utils.get_service_by_appointment(appointment).name
+    master = db_utils.get_master_by_appointment(appointment)
+    master_name = db_utils.get_user_by_master(master).name
+    text = (f"Подробности:\nПроцедура: {service_name}\nМастер: {master_name}\nДата: {appointment.start_time}\n" +
+            f"Статус:{appointment.status}")
+    await callback.message.answer(text=text, reply_markup=kb)
+    await state.set_state(AppointmentsStates.pressing_button)
+    await callback.answer()
+
+@router.callback_query(AppointmentsStates.pressing_button, F.data.startswith('confirm'))
+async def confirm_appointment_handler(callback: CallbackQuery, state: FSMContext):
+    appointment_id = int(callback.data.split('_')[-1])
+    appointment = db_utils.get_appointment_by_id(appointment_id)
+    db_utils.confirm_appointment(appointment)
+    user = db_utils.get_user_by_telegram_id(callback.from_user.id)
+    appointments = db_utils.get_appointments_by_user(user)
+    kb = my_appointments_kb.get_appointments_kb(appointments)
+    await callback.message.answer(text='Запись подтверждена.', reply_markup=kb)
+    await state.set_state(AppointmentsStates.choosing_appointment)
+    await callback.answer()
+
+@router.callback_query(AppointmentsStates.pressing_button, F.data.startswith('cancel'))
+async def cancel_appointment_handler(callback: CallbackQuery, state: FSMContext):
+    appointment_id = int(callback.data.split('_')[-1])
+    appointment = db_utils.get_appointment_by_id(appointment_id)
+    db_utils.cancel_appointment(appointment)
+    user = db_utils.get_user_by_telegram_id(callback.from_user.id)
+    appointments = db_utils.get_appointments_by_user(user)
+    kb = my_appointments_kb.get_appointments_kb(appointments)
+    await callback.message.answer(text='Запись отменена.', reply_markup=kb)
+    await state.set_state(AppointmentsStates.choosing_appointment)
+    await callback.answer()
+
+@router.callback_query(F.data == 'back')
+async def main_menu_handler(callback: CallbackQuery, state: FSMContext):
+    if state:
+        await state.clear()
+    user = db_utils.get_user_by_telegram_id(callback.from_user.id)
+    appointments = db_utils.get_appointments_by_user(user)
+    kb = my_appointments_kb.get_appointments_kb(appointments)
+    await callback.message.answer(text='Мои записи:', reply_markup=kb)
+    await state.set_state(AppointmentsStates.choosing_appointment)
+    await callback.answer()
